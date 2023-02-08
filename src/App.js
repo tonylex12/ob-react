1285c2b6bf7d4e85f1fe55ce78905903af12371e
Antony Huerto <tonylex12@gmail.com>
2023-02-07 19:50:37 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import GreetingF from './components/pure/GreetingF';
 import './App.css';
 
 function App() {
@@ -6,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Componente GreetingF */}
+        <GreetingF 
+          name="Tony"
+        />
       </header>
     </div>
   );
