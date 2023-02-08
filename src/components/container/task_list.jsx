b775c47788a6a964f510dd2924a26b1400705085
Antony Huerto <tonylex12@gmail.com>
2023-02-07 20:22:41 -0500

    fix: title in task list

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -10,7 +10,7 @@ const TaskListComponent = () => {
   return (
     <div>
       <div>
-        Your Tasks:
+        <h1>Your Tasks:</h1>
       </div>
       {/* TODO: Aplicar un for/map para renderizar una lista */}
       <TaskComponent 
